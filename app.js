f2ad98948e95aacbdedce7da293cc564d5372aaf
PavanKuchana <Pavankuchana007@gmail.com>
2021-12-12 06:25:41 +0000

    add

--- app.js
+++ app.js
@@ -1,41 +1,39 @@
 const express = require("express");
 const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
-const path = require("path");
-const cors = require("cors");
-const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
-
-const databasePath = path.join(__dirname, "userData.db");
-
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
+const path = require("path");
 const app = express();
-
 app.use(express.json());
+
 app.use(
   cors({
     origin: "*",
   })
 );
+let db;
+const dbPath = path.join(__dirname, "./userData.db");
 
-let database = null;
-
-const initializeDbAndServer = async () => {
+// Database Init Function
+const initFunc = async () => {
   try {
-    database = await open({
-      filename: databasePath,
+    db = await open({
+      filename: dbPath,
       driver: sqlite3.Database,
     });
     app.listen(process.env.PORT || 6000, () =>
       console.log("Server Running at http://localhost:6000/")
     );
   } catch (error) {
-    console.log(`DB Error: ${error.message}`);
-    process.exit(1);
+    console.log(error.message);
   }
 };
 
-initializeDbAndServer();
+initFunc();
 
+// JWT Authorization middleware function
 const checkToken = (req, res, nxt) => {
   let jwtToken;
   const authHeader = req.headers.authorization;
@@ -46,7 +44,7 @@ const checkToken = (req, res, nxt) => {
     res.status(401);
     res.send("Invalid JWT Token");
   } else {
-    jwt.verify(jwtToken, "MY_SECRET_TOKEN", async (error, payload) => {
+    jwt.verify(jwtToken, "Magic", async (error, payload) => {
       if (error) {
         res.status(401);
         res.send("Invalid JWT Token");
@@ -57,93 +55,98 @@ const checkToken = (req, res, nxt) => {
   }
 };
 
-const validatePassword = (password) => {
-  return password.length > 4;
-};
-
-app.post("/register", async (request, response) => {
-  const { username, name, password, gender, location } = request.body;
-  const hashedPassword = await bcrypt.hash(password, 10);
-  console.log(hashedPassword);
-  const selectUserQuery = `SELECT * FROM user WHERE username = '${username}';`;
-  const databaseUser = await database.get(selectUserQuery);
-
-  if (databaseUser === undefined) {
-    const createUserQuery = `
-     INSERT INTO
-      user (username, name, password, gender, location)
-     VALUES
-      (
-       '${username}',
-       '${name}',
-       '${hashedPassword}',
-       '${gender}',
-       '${location}'  
-      );`;
-    if (validatePassword(password)) {
-      await database.run(createUserQuery);
-      response.send("User created successfully");
-    } else {
-      response.status(400);
-      response.send("Password is too short");
-    }
+// API 1 - user login
+app.post("/login/", async (req, res) => {
+  const { username, password } = req.body;
+  const isUser = await db.get(`
+  select * from user where username = '${username}'
+  `);
+  if (isUser === undefined) {
+    res.status(400);
+    res.send({ error_msg: "Invalid username" });
   } else {
-    response.status(400);
-    response.send("User already exists");
-  }
-});
-
-app.post("/login", async (request, response) => {
-  const { username, password } = request.body;
-  const selectUserQuery = `SELECT * FROM user WHERE username = '${username}';`;
-  const databaseUser = await database.get(selectUserQuery);
-
-  if (databaseUser === undefined) {
-    response.status(400);
-    response.send("Invalid user");
-  } else {
-    const isPasswordMatched = await bcrypt.compare(
-      password,
-      databaseUser.password
-    );
-    if (isPasswordMatched === true) {
-      const payload = {
-        username: username,
-      };
-      let jwtToken = jwt.sign(payload, "MY_SECRET_TOKENN");
-      response.send({ jwtToken });
-      response.status(200);
+    if (await bcrypt.compare(password, isUser.password)) {
+      let payload = { username: username, fullname: isUser.full_name };
+      console.log(`${username} is logged in`);
+      let jwt_token = jwt.sign(payload, "Magic");
+      res.send({ jwt_token });
+      res.status(200);
     } else {
-      response.status(400);
-      response.send("Invalid password");
+      res.status(400);
+      res.send({ error_msg: "Invalid password" });
     }
   }
 });
 
-app.post("/users/", checkToken, async (request, response) => {
-  const userDetails = request.body;
-  const values = userDetails.map(
-    (each) => `('${each.userId}', '${each.id}', '${each.title}','${each.body}')`
-  );
-
-  const valuesString = values.join(",");
-
-  const addUserQuery = `
-    INSERT INTO
-      userData (user_id,id,title,body)
-    VALUES
-       ${valuesString};`;
-
-  const dbResponse = await db.run(addUserQuery);
-  const ItemId = dbResponse.lastID;
-  response.send({ ItemId: ItemId });
+app.get("/get-user", async (req, res) => {
+  const header = req.headers.authorization;
+  const myToken = header.split(" ")[1];
+  jwt.verify(myToken, "Magic", (error, decodedToken) => {
+    if (error) {
+      console.log(error.message);
+      res.status(400);
+      res.send({ error_msg: error.message });
+    } else {
+      res.status(200);
+      res.send(decodedToken);
+    }
+  });
 });
 
-app.get("/usersData/", checkToken, async (req, res) => {
-  const getFromDatabase = await database.all(`
-    SELECT * FROM userData
+//API 2 - send data to database from received file
+app.post("/data/", checkToken, async (req, res) => {
+  const gotData = req.body;
+  gotData.forEach(async (each) => {
+    const { userId, id, title, body } = each;
+    const getDis = await db.run(`
+          INSERT INTO uploaded (user_id, id, title, body)
+          VALUES ('${userId}','${id}','${title}','${body}');
+      `);
+  });
+  res.send("success");
+});
+
+//API 3 - Register new user
+app.post("/register/", async (req, res) => {
+  const { username, fullname, password } = req.body;
+  const isExist = await db.get(`
+    select * from user where username = '${username}'
+  `);
+  if (isExist === undefined) {
+    if (password.length < 5) {
+      res.status(400);
+      res.send({ error_msg: "Password is too short" });
+      console.log("Password is too short");
+    } else {
+      const encryptedPass = await bcrypt.hash(password, 10);
+      const addUser = await db.run(`
+            insert into user (username,full_name,password) values ('${username}','${fullname}','${encryptedPass}');
+        `);
+      res.status(200);
+      res.send({ msg: "User created successfully" });
+      console.log("User created successfully");
+    }
+  } else {
+    res.status(400);
+    res.send({ error_msg: "User already exists" });
+    console.log("User already exists");
+  }
+});
+
+//API 4 - send stored data from database to frontend
+app.get("/return/", checkToken, async (req, res) => {
+  const getFromDatabase = await db.all(`
+    SELECT * FROM uploaded
     `);
   res.send({ getFromDatabase });
 });
+
+// test api
+app.get("/test", (req, res) => {
+  console.log("test done");
+  res.send("test text appering!!");
+});
+
+//ok nothing deleted
 
 module.exports = app;
